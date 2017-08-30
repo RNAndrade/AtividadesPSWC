8549c9250c98f40d5d87b1abcecb84b26ea997ce
RNAndrade <rnandrade@gmail.com>
2017-08-30 20:05:29 -0300

    Pronto

--- PSWC.AtividadeAprof01/src/basic/ControleRemotoUniversal.java
+++ PSWC.AtividadeAprof01/src/basic/ControleRemotoUniversal.java
@@ -1,39 +1,12 @@
 package basic;
 
-public class ControleRemotoUniversal {
-	
-	private AparelhoSom aparelhoSom;
-	private Televisao tv;
-	private BlurayPlayer bluray;
-	
-	public ControleRemotoUniversal() {
-		super();
-		AparelhoSom aparelhoSom = new AparelhoSom();
-		Televisao tv = new Televisao();
-		BlurayPlayer bluray = new BlurayPlayer();
+public class ControleRemotoUniversal extends Eletronico {
+
+	public void ligar(Eletronico eletronico) {
+		System.out.println("Ligando: " + eletronico.getClass().getName());
 	}
 
-	public AparelhoSom getAparelhoSom() {
-		return aparelhoSom;
-	}
-
-	public void setAparelhoSom(AparelhoSom aparelhoSom) {
-		this.aparelhoSom = aparelhoSom;
-	}
-
-	public Televisao getTv() {
-		return tv;
-	}
-
-	public void setTv(Televisao tv) {
-		this.tv = tv;
-	}
-
-	public BlurayPlayer getBluray() {
-		return bluray;
-	}
-
-	public void setBluray(BlurayPlayer bluray) {
-		this.bluray = bluray;
+	public void desligar(Eletronico eletronico) {
+		System.out.println("Desligando: " + eletronico.getClass().getName());
 	}
 }
--- PSWC.AtividadeAprof01/src/basic/TesteControleRemoto.java
+++ PSWC.AtividadeAprof01/src/basic/TesteControleRemoto.java
@@ -3,9 +3,10 @@ package basic;
 public class TesteControleRemoto {
 
 	public static void main(String[] args) {
-		
+
 		ControleRemotoUniversal controle = new ControleRemotoUniversal();
-		
-		
+		controle.ligar(new Televisao());
+		controle.ligar(new BlurayPlayer());
+
 	}
 }
