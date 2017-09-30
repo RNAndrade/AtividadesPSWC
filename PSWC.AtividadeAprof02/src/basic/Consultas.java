dd252138ac718ba1cf38e48c85ef3b6164a155be
RNAndrade <rnandrade@gmail.com>
2017-09-29 23:53:53 -0300

    Último commit

--- PSWC.AtividadeAprof02/src/basic/Consultas.java
+++ PSWC.AtividadeAprof02/src/basic/Consultas.java
@@ -6,22 +6,25 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-
 public class Consultas {
 	public static void main(String[] args) {
 		EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("unidadePersistencia");
 		EntityManager manager = managerFactory.createEntityManager();
 		DAOJogador DAO = new DAOJogador();
+		ArrayList<Jogador> players = new ArrayList<Jogador>();
+
+		// -------------- Letra "a" --------------------
 		Jogador player = DAO.pesquisarCamisa(manager, "Cruzeiro", 3);
 		System.out.println(player.getNome());
-		
-		ArrayList<Jogador> players = new ArrayList<Jogador>();
+
+		// -------------- Letra "b" --------------------
 		players = (ArrayList<Jogador>) DAO.pesquisarJogadorCartaoVermelho(manager, "São Paulo");
 		for (Jogador jogador : players) {
 			System.out.println(jogador.getNome());
 		}
-		
+
+		// -------------- Letra "c" --------------------
 		System.out.println(DAO.qtdJogadorTime(manager, "SP", "S%"));
-		
+
 	}
 }
